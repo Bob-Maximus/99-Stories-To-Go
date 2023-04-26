1f6a647a6fcd2fa0de05ee6479751a44a2fb5e5c
Bob-Maximus <yousefelkabbany@gmail.com>
2023-04-25 20:44:44 -0400

    started the framework for changing areas

---
+++ 99 storeys to go (actual game stuff)/Assets/enviroment/enterable tings/EnterableData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnterableData", menuName = "EnterableData")]
+public class EnterableData : ScriptableObject
+{
+    public List<BoxCollider> EnterTrigger, <
+}
