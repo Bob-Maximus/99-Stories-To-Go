ecd13f76ffbf33c9aea826f08094fcbd638ddaa3
Bob-Maximus <yousefelkabbany@gmail.com>
2023-04-26 11:09:07 -0400

    started adding changing scenes stuff

--- 99 storeys to go (actual game stuff)/Assets/enviroment/enterable tings/EnterableData.cs
+++ 99 storeys to go (actual game stuff)/Assets/enviroment/enterable tings/EnterableData.cs
@@ -1,9 +1,24 @@
-using System.Collections;
+using Eflatun.SceneReference;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "EnterableData", menuName = "EnterableData")]
-public class EnterableData : ScriptableObject
+public class ChangeLevel : ScriptableObject
 {
-    public List<BoxCollider> EnterTrigger, <
+    public List<MyClass> RoomEnterableData;    
+}
+
+[System.Serializable]
+public class MyClass
+{
+    public BoxCollider changeLevelTrigger;
+    public Vector3 spawnPoint;
+    public SceneReference newLevel;
+
+    public MyClass(BoxCollider changeLevelTrigger, Vector3 spawnPoint, SceneReference newLevel)
+    {
+        this.changeLevelTrigger = changeLevelTrigger;
+        this.changeLevelTrigger = changeLevelTrigger;
+        this.newLevel = newLevel;
+    }
 }
